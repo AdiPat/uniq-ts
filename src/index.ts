9933fa51155902bad5e4e902b0e1392440e8a65c
Aditya Patange <contact.adityapatange@gmail.com>
2024-12-02 00:05:35 +0530

    zuniq: empty file check and error if file doesn't exist

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import fs from "fs/promises";
 export async function zuniq({ filePath }: { filePath: string }): Promise<{
   out: string;
 }> {
+  await assertFileExists(filePath);
   const fileContent = await fs.readFile(filePath, "utf-8");
   const lines = fileContent.split("\n");
   const uniqueLines = lines.filter((line, index) => {
@@ -10,3 +11,11 @@ export async function zuniq({ filePath }: { filePath: string }): Promise<{
   });
   return { out: uniqueLines.join("\n") };
 }
+
+const assertFileExists = async (filePath: string) => {
+  try {
+    await fs.access(filePath);
+  } catch (error) {
+    throw new Error(`Error: Invalid file path '${filePath}'`);
+  }
+};
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -12,4 +12,16 @@ describe("zuniq", () => {
     const { out } = await zuniq({ filePath: testFilePath });
     expect(out).toEqual(["line1", "line2", "line3", "line4"].join("\n"));
   });
+
+  it("should return empty string if file is empty", async () => {
+    const { out } = await zuniq({ filePath: "./test_data/empty.txt" });
+    expect(out).toEqual("");
+  });
+
+  it("should throw an error if file does not exist", async () => {
+    const promise = zuniq({ filePath: "./test_data/non_existent.txt" });
+    await expect(promise).rejects.toThrow(
+      "Error: Invalid file path './test_data/non_existent.txt'"
+    );
+  });
 });
