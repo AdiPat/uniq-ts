87e188762e5203c6aa96e5fc044749c8c6a02985
Aditya Patange <contact.adityapatange@gmail.com>
2024-12-02 00:02:39 +0530

    zuniq: add basic functionality with file path

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
-export function zuniq() {
-  throw new Error("Not implemented");
+import fs from "fs/promises";
+
+export async function zuniq({ filePath }: { filePath: string }): Promise<{
+  out: string;
+}> {
+  const fileContent = await fs.readFile(filePath, "utf-8");
+  const lines = fileContent.split("\n");
+  const uniqueLines = lines.filter((line, index) => {
+    return index === 0 || lines[index - 1] !== line;
+  });
+  return { out: uniqueLines.join("\n") };
 }
--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -2,7 +2,14 @@ import { describe, expect, it } from "vitest";
 import { zuniq } from "../index";
 
 describe("zuniq", () => {
+  const testFilePath = "./test_data/test.txt";
+
   it("should be defined", () => {
     expect(zuniq).toBeDefined();
+  });
+
+  it("should accept a file path and return a unique list of string where adjacent repeated strings are removed", async () => {
+    const { out } = await zuniq({ filePath: testFilePath });
+    expect(out).toEqual(["line1", "line2", "line3", "line4"].join("\n"));
   });
 });
